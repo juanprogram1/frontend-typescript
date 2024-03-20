5143ea9a1fda8ed7508a346b1b09962b05d9ee88
Juan David Carrillo Anillo <anilloj62@gmail.com>
2024-03-20 16:20:50 -0500

    Merge branch Prueba => main

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export const  sumar = (a: number, b: number) => {
+const sumar = (a: number, b: number) => {
     return a + b;
 }
+
+console.log(sumar(1, 1));
+
+const muchoText: string[] = ['Juan', 'David', 'pepito'];
+
+console.log(muchoText[0]);
+console.log(muchoText[2]);
--- src/main.ts
+++
@@ -1,1 +0,0 @@
-import {sumar} from "./index";
