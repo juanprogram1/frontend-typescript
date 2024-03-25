12b4a2581ebf9adef416afb1e557a20ce917d1f3
Juan David Carrillo Anillo <anilloj62@gmail.com>
2024-03-25 14:14:16 -0500

    Boirplate Completed - JuanDavidCarrilloAnillo

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+const world = "mundo";
+`Hola ${world}`;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLE1BQU0sS0FBSyxHQUFHLE9BQU8sQ0FBQztBQUN0QixRQUFRLEtBQUssRUFBRSxDQUFDIn0=
